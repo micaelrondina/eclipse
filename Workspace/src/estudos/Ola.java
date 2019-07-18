9599bc2dcdbf4080ad3e1cb6a2016bedd8f7ab64
Micael ROndina <mrondina@dxc.com>
2019-07-18 12:18:41 -0300

    Adicionei alguma coisa ai

--- Workspace/src/estudos/Ola.java
+++ Workspace/src/estudos/Ola.java
@@ -1,7 +1,15 @@
 package estudos;
 
 public class Ola {
-	public static void main (String args[]) {
+	public static void main(String args[]) {
 		System.out.println("Ola");
+		int n1[] = new int[3];
+		n1[0] = 3;
+		n1[1] = 2;
+		n1[2] = 3;
+
+		for (int aux : n1) {
+			System.out.println(aux);
+		}
 	}
-}	
+}
